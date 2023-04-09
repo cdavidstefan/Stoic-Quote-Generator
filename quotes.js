58994fcbc1969666eeb8eacfbb1651dd5e407ebe
David <ciucadavidstefan@gmail.com>
2023-04-09 09:38:05 +0300

    initial commit

---
+++ quotes.js
@@ -0,0 +1,42 @@
+const localQuotes = [
+  {
+    text: 'Genius is one percent inspiration and ninety-nine percent perspiration.',
+    author: 'Thomas Edison',
+  },
+  {
+    text: 'You can observe a lot just by watching.',
+    author: 'Yogi Berra',
+  },
+  {
+    text: 'A house divided against itself cannot stand.',
+    author: 'Abraham Lincoln',
+  },
+  {
+    text: 'Difficulties increase the nearer we get to the goal.',
+    author: 'Johann Wolfgang von Goethe',
+  },
+  {
+    text: 'Însușirile specifice unui suflet rațional sunt: se autoanalizează... si se autodetermină așa cum voiește. Își culege roadele... Își îndeplinește scopul său propriu.',
+    author: 'Marcus Aurelius',
+  },
+  {
+    text: 'Zenon spunea totodată că nimic nu este mai ostil stăpânirii ferme a cunoașterii decât amăgirea de sine.',
+    author: 'Diogene Laerțiu',
+  },
+  {
+    text: 'Debarasează-te de opiniile vanitoase, fiindcă este imposibil pentru o persoană să învețe ceva ce consideră că știe deja.',
+    author: 'Epictet, Diatribe, 2.17.1',
+  },
+  {
+    text: 'Nici vorba rea și nici bățul care te lovește nu te doare, ci părerea ta despre dânsele. Și așa, la începutul oricărei dureri, vei găsi îndată nu un lucru, ci părerea ta despre acel lucru. Caută dar, înainte de toate, să nu te răpească imediat părerea căci dacă tu capeți puțin răgaz de găndire, mai ușor îți vei păstra cumpătul.',
+    author: 'Epictet, Manualul, 20',
+  },
+  {
+    text: 'Între stimul și răspuns există un spațiu, în acel spațiu rezidă forța noastră de a ne alege reacțiile, iar de reacțiile noastre depind bunăstarea și fericirea noastră.',
+    author: null,
+  },
+  {
+    text: 'They say the true hell is when the person you are meets the person you could have been.',
+    author: null,
+  },
+];
--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-console.log('testing');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// let apiQuotes = [];
+
+// Show Loading
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show new quote
+function newQuote() {
+    showLoadingSpinner();
+    // Pick a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+
+    // CHeck if Author field is blanck and replace it with 'Unknown';
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    
+    // Check the Quote length to determine styling
+    if (quote.text.length > 90) {
+        quoteText.classList.add('long-quote');
+    }   else {
+        quoteText.classList.remove('long-quote');
+    }
+    
+    // Set quote, hide loader
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+// Get quotes from API
+
+// async function getQuotes () {
+//     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch(error) {
+//         // Catch Error Here
+//     }
+// }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+
+// On Load
+// getQuotes();
+
+newQuote();
